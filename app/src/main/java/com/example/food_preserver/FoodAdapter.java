748ac7ad39549bb150947caecae1875f80f0aedb
Apexic <64813809+Apexic@users.noreply.github.com>
2020-10-07 17:45:55 -0700

    database can now support images

--- app/src/main/java/com/example/food_preserver/FoodAdapter.java
+++ app/src/main/java/com/example/food_preserver/FoodAdapter.java
@@ -5,13 +5,14 @@ package com.example.food_preserver;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 public class FoodAdapter extends FirestoreRecyclerAdapter<FoodItem, FoodAdapter.NoteHolder> {
     public FoodAdapter(@NonNull FirestoreRecyclerOptions<FoodItem> options) {
@@ -22,6 +23,7 @@ public class FoodAdapter extends FirestoreRecyclerAdapter<FoodItem, FoodAdapter.
         holder.textViewTitle.setText(model.getTitle());
         holder.textViewDescription.setText(model.getDescription());
         holder.textViewPriority.setText(String.valueOf(model.getPriority()));
+        Picasso.get().load(model.getPicture()).into(holder.myImageView);
     }
     @NonNull
     @Override
@@ -34,11 +36,14 @@ public class FoodAdapter extends FirestoreRecyclerAdapter<FoodItem, FoodAdapter.
         TextView textViewTitle;
         TextView textViewDescription;
         TextView textViewPriority;
+        ImageView myImageView;
         public NoteHolder(View itemView) {
             super(itemView);
             textViewTitle = itemView.findViewById(R.id.text_view_title);
             textViewDescription = itemView.findViewById(R.id.text_view_description);
             textViewPriority = itemView.findViewById(R.id.text_view_priority);
+            myImageView = itemView.findViewById(R.id.myImageView);
+
         }
     }
 }
--- app/src/main/java/com/example/food_preserver/FoodItem.java
+++ app/src/main/java/com/example/food_preserver/FoodItem.java
@@ -4,15 +4,17 @@ public class FoodItem {
     private String title;
     private String description;
     private int priority;
+    private String picture;
 
     public FoodItem() {
         //empty constructor needed
     }
 
-    public FoodItem(String title, String description, int priority) {
+    public FoodItem(String title, String description, int priority, String picture) {
         this.title = title;
         this.description = description;
         this.priority = priority;
+        this.picture = picture;
     }
 
     public String getTitle() {
@@ -26,5 +28,7 @@ public class FoodItem {
     public int getPriority() {
         return priority;
     }
+
+    public String getPicture() { return picture; }
 }
 
